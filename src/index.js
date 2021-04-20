02541930e35114c0fb990fa00358f7844934ef92
Vovchik_tkd <vovchik_tkd@Macintoshs-MacBook-Pro.local>
2021-04-20 23:41:22 +0300

    read endpoint added

--- src/index.js
+++ src/index.js
@@ -15,7 +15,27 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e.message);
     })
-})
+});
+
+app.get('/users', (req, res) => {
+    User.find({}).then(users => {
+        res.send(users);
+    }).catch(e => {
+        res.status(500).send(e);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id).then(user => {
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    }).catch(() => {
+        res.status(500).send();
+    });
+});
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
@@ -24,8 +44,28 @@ app.post('/tasks', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e.message);
     })
-})
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then(tasks => {
+        res.send(tasks);
+    }).catch(e => {
+        res.status(500).send(e);
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+    Task.findById(_id).then(task => {
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    }).catch(() => {
+        res.status(500).send();
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running in port ${port}`);
-})
+});
